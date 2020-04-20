28e904eb82e238ac4b0e724786d19af3f8bff71f
Kinga <kinga123kon@gmail.com>
2020-04-20 16:35:16 +0200

    moja gra memory

---
+++ content/src/js/app/main.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider, $locationProvider) {
+    
+    $locationProvider.html5Mode(true);
+});
+
+Date.prototype.addHours = function (h) {
+    this.setTime(this.getTime() + (h * 60 * 60 * 1000));
+    return this;
+}
